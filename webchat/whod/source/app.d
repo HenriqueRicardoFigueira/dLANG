08a489251e2f5472a483bbe6c7d9f2b49b116098
henrislip <henriquefigueria@alunos.utfpr.edu.br>
2019-05-01 17:51:14 -0300

    chat funcionando protocolo http

--- webchat/whod/source/app.d
+++ webchat/whod/source/app.d
@@ -1,52 +1,27 @@
-import vibe.vibe;
+import vibe.d;
 
+class WebChats
+{
+    private Room[string] m_rooms;
 
+    void get(HTTPServerRequest req, HTTPServerResponse res)
+    {
+        res.render!("index.dt");
+    }
 
-final class WebChat {
-	private Room[string] m_rooms;
-	void get()
-	{
-		render!"index.dt";
-	}
-
-
-	void getRoom(string id, string name)
-	{
-		auto messages = getOrCreateRoom(id).messages;
+    void getRoom(string id, string name)
+    {
+        auto messages = getOrCreateRoom(id).messages;
 		render!("room.dt", id, name, messages);
-	}
+    }
 
-	void postRoom(string id, string name, string message)
+    void postRoom(string id, string name, string message)
 	{
 		if (message.length)
 			getOrCreateRoom(id).addMessage(name, message);
 		redirect("room?id="~id.urlEncode~"&name="~name.urlEncode);
 	}
-
-
-
-	void getWS(string room, string name, scope WebSocket socket)
-	{
-		auto r = getOrCreateRoom(room);
-
-		runTask({
-
-			auto next_message = r.messages.length;
-
-			while (socket.connected) {
-				while (next_message < r.messages.length)
-					socket.send(r.messages[next_message++]);
-				r.waitForMessage(next_message);
-		}});
-		while (socket.waitForData) {
-			auto message = socket.receiveText();
-			if (message.length) r.addMessage(name, message);
-		}
-
-
-		
-	}
-	private Room getOrCreateRoom(string id)
+    private Room getOrCreateRoom(string id)
 	{
 		if (auto pr = id in m_rooms) return *pr;
 		return m_rooms[id] = new Room;
@@ -55,51 +30,28 @@ final class WebChat {
 
 final class Room {
 	string[] messages;
-	LocalManualEvent messageEvent;
-
-	this()
-	{
-		messageEvent = createManualEvent();
-	}
 
 	void addMessage(string name, string message)
 	{
 		messages ~= name ~ ": " ~ message;
-		messageEvent.emit();
-	}
-
-	void waitForMessage(size_t next_message)
-	{
-		while (messages.length <= next_message)
-			messageEvent.wait();
 	}
 }
 
-
-
-
+void helloWorld(HTTPServerRequest req, HTTPServerResponse res)
+{
+    res.render!("index.dt");
+}
 
 void main()
 {
-	auto router = new URLRouter; // instancia do criador de rotas
-	router.registerWebInterface(new WebChat);// transforma o objeto webchat em rota
-	router.get("*", serveStaticFiles("public/"));// chama o arquivo java script 
+    auto router = new URLRouter;
+    router.registerWebInterface(new WebChats);
+    //router.get("*", serveStaticFiles("views/");
 
-
-
-
-	auto settings = new HTTPServerSettings; //instancia do servidor http
-	settings.port = 8080;// defina porta 8080
-	settings.bindAddresses = ["::1", "127.0.0.1"]; //endereço de rede
-
-
-
-	listenHTTP(settings, router);
-	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
-	runApplication();
-}
-
-void hello(HTTPServerRequest req, HTTPServerResponse res)
-{
-	res.writeBody("Hello, World!");
+    auto settings = new HTTPServerSettings;
+    // Needed for SessionVar usage.
+    settings.sessionStore = new MemorySessionStore;
+    settings.port = 8080;
+    listenHTTP(settings, router);
+    runApplication();
 }
