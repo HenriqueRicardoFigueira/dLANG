8ea4af567da7a8d3e17bc1d91c7650b0188812b5
HCelante <higor.celante@gmail.com>
2019-05-05 14:02:46 -0300

    correção

--- webchat/whod/source/app.d
+++ webchat/whod/source/app.d
@@ -4,23 +4,49 @@ class WebChats
 {
     private Room[string] m_rooms;
 
-    void get(HTTPServerRequest req, HTTPServerResponse res)
+    void get(HTTPServerResponse res)
     {
         res.render!("index.dt");
-    }
-
-    void getRoom(string id, string name)
+        
+    }   
+    
+    void getNewplayer(HTTPServerResponse res)
     {
-        auto messages = getOrCreateRoom(id).messages;
-		render!("room.dt", id, name, messages);
+        res.render!("newplayer.dt");
     }
 
+    void getRoom(string id, string name, string tema)
+    {   
+        string[] members;
+        auto messages = getOrCreateRoom(id).messages;
+        members = m_rooms[id].members;
+        bool x = m_rooms[id].checkPlayer(name);
+        if(x == false){
+            managementRoom(id, name, tema);
+            render!("room.dt", id, name, messages, members,tema);
+        } else {
+            
+            render!("room.dt", id, name, messages, members,tema);
+        }
+    }
+
+    void managementRoom(string id, string name, string tema){
+        
+        auto px = new Player(name, id, 0, true);
+        string pname = px.name;
+        m_rooms[id].addMembers(pname);
+        m_rooms[id].tema = tema;
+        
+    };
+   
     void postRoom(string id, string name, string message)
 	{
 		if (message.length)
 			getOrCreateRoom(id).addMessage(name, message);
-		redirect("room?id="~id.urlEncode~"&name="~name.urlEncode);
+        string tema = m_rooms[id].tema;
+		redirect("room?id="~id.urlEncode~"&name="~name.urlEncode~"&tema="~tema.urlEncode);
 	}
+
     private Room getOrCreateRoom(string id)
 	{
 		if (auto pr = id in m_rooms) return *pr;
@@ -28,19 +54,46 @@ class WebChats
 	}
 }
 
+final class Player{
+    string name;
+    string room;
+    int score;
+    bool master;
+
+    this(string name, string room, int score, bool master){
+        this.name = name;
+        this.room = room;
+        this.score = score;
+        this.master = master;
+    }
+}
+
 final class Room {
 	string[] messages;
+    string tema;
+    string[] members;
+    
 
 	void addMessage(string name, string message)
 	{
 		messages ~= name ~ ": " ~ message;
 	}
+
+    void addMembers(string name)
+    {
+        members ~= name;
+    }
+
+    bool checkPlayer(string name)
+    {
+        foreach(string x; members){
+            if(x == name)
+                return true;
+        }
+        return false;
+    }
 }
 
-void helloWorld(HTTPServerRequest req, HTTPServerResponse res)
-{
-    res.render!("index.dt");
-}
 
 void main()
 {
